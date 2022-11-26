de0574b2d44c2f5cb03f7dafc5a3a5d19566d773
Mikhail Yudin <fagci.nsk@gmail.com>
2022-11-26 20:58:27 +0700

    refactor

--- mist.py
+++ mist.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from ipaddress import IPv4Address
-from random import randrange
+from random import getrandbits
 from socket import setdefaulttimeout, socket
+from socket import inet_ntoa
+from struct import pack
 from subprocess import PIPE, Popen
 from sys import stderr
 from threading import BoundedSemaphore, Thread
-from random import getrandbits
-from socket import inet_ntoa
-from struct import pack
 
 
 class Handler:
@@ -31,7 +29,7 @@ class Handler:
             p = Popen([cmd, ip, str(port)], stdout=PIPE, stderr=PIPE)
             out, err = p.communicate()
             if out:
-                print(out.decode())
+                print(out.decode(), end='')
             if err:
                 print('[E] stderr:', file=stderr)
                 print(err.decode(), file=stderr)
@@ -61,27 +59,6 @@ class Handler:
 
 
 class Worker(Thread):
-    @classmethod
-    def random_wan_ip(cls):
-        while True:
-            int_ip = 0x01000000 + getrandbits(32) % 0xfeffffff
-            if (0xe0000000 <= int_ip < 0xffffffff
-                    or 0xA000000 <= int_ip < 0xb000000
-                    or 0x7F000000 <= int_ip < 0x80000000
-                    or 0x64400000 <= int_ip < 0x64800000
-                    or 0xAC100000 <= int_ip < 0xac200000
-                    or 0xC6120000 <= int_ip < 0xc6140000
-                    or 0xA9FE0000 <= int_ip < 0xa9ff0000
-                    or 0xC0A80000 <= int_ip < 0xc0a90000
-                    or 0xC0000000 <= int_ip < 0xc0000100
-                    or 0xC0000200 <= int_ip < 0xc0000300
-                    or 0xc0586300 <= int_ip < 0xc0586400
-                    or 0xC6336400 <= int_ip < 0xc6336500
-                    or 0xCB007100 <= int_ip < 0xcb007200
-                    or 0xe9fc0000 <= int_ip < 0xe9fc0100):
-                continue
-            return inet_ntoa(pack('>I', int_ip))
-
     def __init__(self, port, handler) -> None:
         super().__init__(daemon=True)
         self.port = port
@@ -93,6 +70,27 @@ class Worker(Thread):
             with socket() as s:
                 if s.connect_ex((ip, self.port)) == 0:
                     self.handler.handle(ip, self.port, s)
+
+    @classmethod
+    def random_wan_ip(cls):
+        while True:
+            int_ip = 0x01000000 + getrandbits(32) % 0xfeffffff
+            if (0xe0000000 <= int_ip < 0xffffffff
+                or 0xA000000 <= int_ip < 0xb000000
+                or 0x7F000000 <= int_ip < 0x80000000
+                or 0x64400000 <= int_ip < 0x64800000
+                or 0xAC100000 <= int_ip < 0xac200000
+                or 0xC6120000 <= int_ip < 0xc6140000
+                or 0xA9FE0000 <= int_ip < 0xa9ff0000
+                or 0xC0A80000 <= int_ip < 0xc0a90000
+                or 0xC0000000 <= int_ip < 0xc0000100
+                or 0xC0000200 <= int_ip < 0xc0000300
+                or 0xc0586300 <= int_ip < 0xc0586400
+                or 0xC6336400 <= int_ip < 0xc6336500
+                or 0xCB007100 <= int_ip < 0xcb007200
+                or 0xe9fc0000 <= int_ip < 0xe9fc0100):
+                continue
+            return inet_ntoa(pack('>I', int_ip))
 
 
 def main(args):
